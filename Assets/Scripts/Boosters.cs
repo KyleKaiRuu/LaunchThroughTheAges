6973f5d5762eb16164b0796dd13eeb8e7da1db6d
Gavin Coffelt <gavincoffelt@yahoo.com>
2019-05-17 09:09:14 -0700

    Working Level

---
+++ Assets/Scripts/Boosters.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boosters : MonoBehaviour {
+    private bool isUsed = false;
+   
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(!isUsed && other.gameObject.tag.Equals("Player"))
+        {
+            other.gameObject.GetComponent<Rigidbody>().velocity = Vector3.up * other.gameObject.GetComponent<TestControls>().velocity;
+            isUsed = true;
+            gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/Hazards.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hazards : MonoBehaviour {
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag.Equals("Player"))
+        {
+            print("You DEAD AS FUCK!");
+        }
+    }
+}
